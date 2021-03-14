fed4adb3b9552b7a6ed9bbd70aad65f49e94df71
Busani <busani@plutosave.com>
2021-03-14 14:13:10 +0200

    Built initial architecture

---
+++ ordinance.py
@@ -0,0 +1,55 @@
+import os
+import eyed3
+
+def sort_music_in_cwd():
+    files = os.listdir()
+    music_files = [file for file in files if file.endswith('.mp3')]
+    print(f'Found songs: {music_files}')
+
+    music_metadata = []
+
+    for song in music_files:
+        try:
+            audiofile = eyed3.load(song)                          
+            if audiofile:                                                        
+                music_metadata.append({                        
+                    'artist': audiofile.tag.artist,      
+                    'title': audiofile.tag.title,        
+                    'album': audiofile.tag.album,            
+                    'track_number': audiofile.tag.track_num,
+                    'file_path': song
+                })
+        except Exception as e:
+            print(e)
+            pass
+
+    for track in music_metadata:
+        try:
+            if track['artist'] != None and track['album'] != None:
+                if not os.path.exists(track['artist']):    
+                    os.mkdir(f"{track['artist']}/")  
+                    print(f"Creating {track['artist']}/")
+
+                if not os.path.exists(f"{track['artist']}/{track['album']}"):    
+                    os.mkdir(f"{track['artist']}/{track['album']}")
+                    print(f"Creating {track['artist']}/{track['album']}/")
+    
+                print(f"Moving {track['file_path']} to {track['artist']}/{track['album']}")
+                os.rename(track['file_path'], f"{track['artist']}/{track['album']}/{track['file_path']}")
+
+            if track['artist'] != None and track['album'] == None:
+                if not os.path.exists(track['artist']):    
+                    os.mkdir(f"{track['artist']}/")  
+                    print(f"Creating {track['artist']}/")
+         
+                print(f"Moving {track['file_path']} to {track['artist']}/")
+                os.rename(track['file_path'], f"{track['artist']}/{track['file_path']}")
+  
+        except Exception as e:
+            print(e)
+            pass
+
+
+if __name__ == '__main__':
+    sort_music_in_cwd()
+
